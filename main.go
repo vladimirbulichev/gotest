6bab61b62211312e1f3b426396764ab278066269
Булычев Владимир <v.bulichev@original-group.ru>
2020-04-27 14:17:08 +0400

    added total word count to watchdog class to make locks on updates

--- main.go
+++ main.go
@@ -12,11 +12,12 @@ import (
 
 type watchDog struct {
 	avail int
+	total int
 	sync.Mutex
 }
 
 func newWatchDog() *watchDog {
-	return &watchDog{avail: 5}
+	return &watchDog{avail: 5, total: 0}
 }
 
 func (w *watchDog) Add() {
@@ -25,25 +26,25 @@ func (w *watchDog) Add() {
 	w.avail--
 }
 
-func (w *watchDog) CheckAvail() (bool){
+func (w *watchDog) CheckAvail() bool {
 	w.Lock()
 	defer w.Unlock()
-	return w.avail>0
+	return w.avail > 0
 }
 
-func (w *watchDog) Release()  {
+func (w *watchDog) Release(c int) {
 	w.Lock()
 	defer w.Unlock()
 	w.avail++
+	w.total += c
 }
 
-func main(){
+func main() {
 	in := os.Stdin
-	total := 0
 	scanner := bufio.NewScanner(in)
 	wd := newWatchDog()
 	var wg sync.WaitGroup
-	for scanner.Scan(){
+	for scanner.Scan() {
 		urlScan := scanner.Text()
 		for !wd.CheckAvail() {
 			fmt.Print("Wait\r")
@@ -51,18 +52,19 @@ func main(){
 		wd.Add()
 		wg.Add(1)
 		go func(url string) {
-			total += wordCount(url)
-			wg.Done()
-			wd.Release()
+			defer wg.Done()
+			wd.Release(wordCount(url))
 		}(urlScan)
 	}
 	wg.Wait()
-	fmt.Println("Total", total)
+	wd.Lock()
+	fmt.Println("Total", wd.total)
+	wd.Unlock()
 }
 
 func wordCount(url string) (c int) {
 	resp, err := http.Get(url)
-	if err!=nil{
+	if err != nil {
 		return 0
 	}
 	body, _ := ioutil.ReadAll(resp.Body)
